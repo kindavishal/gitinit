7118889d4ed822c8826c42141a9a8cc608ac770a
kindavishal <vishaldas.95@gmail.com>
2019-09-01 10:02:48 +0530

    Footer Update

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,8 +3,7 @@ import Link from 'gatsby-link'
 
 const Footer = ({ author, profile }) => (
     <footer>
-        Made with <i className="em em-heart" style={{width: '15px', height: '15px'}}></i>
-        <Link to={profile}>{' @'+author}</Link>
+        Made with <i className="em em-heart" style={{width: '15px', height: '15px'}}></i> <a href={'https://github.com/haxzie'} target='_blank'>@haxzie</a> and remixed with <i className="em em-sparkles" style={{width: '15px', height: '15px'}}></i> by <a href={'https://github.com/kindavishal'} target='_blank'>@kindavishal</a>
     </footer>
 )
 
